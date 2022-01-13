bcaf5e16071d50f367a64b94bad33ba8da611c5f
Ciremun <Ciremun@yandex.ru>
2022-01-14 00:16:45 +0300

    chapter switch

--- source/main.cpp
+++ source/main.cpp
@@ -3,6 +3,8 @@
 #include "process.hpp"
 #include "util.hpp"
 
+static DWORD chapter = 2;
+
 int main()
 {
     DWORD process_id = GetProcId("Inscryption.exe");
@@ -15,13 +17,21 @@ int main()
     const uintptr_t offsets[] = { 0x20, 0x618, 0x3c, 0x10, 0x8, 0x18, 0x14 };
     while (1)
     {
-        uintptr_t damage_dealt_address = external_multi_level_pointer_dereference(process_handle, base, sizeof(uint32_t), offsets);
-        if (damage_dealt_address != 0)
+        switch (chapter)
         {
-            DWORD damage_dealt;
-            external_memory_read(process_handle, (LPVOID)damage_dealt_address, &damage_dealt, sizeof(uint32_t));
-            if (damage_dealt == 0)
-                external_memory_patch(process_handle, (LPVOID)damage_dealt_address, &new_damage_dealt, sizeof(uint32_t));
+            case 2:
+            {
+                uintptr_t damage_dealt_address = external_multi_level_pointer_dereference(process_handle, base, sizeof(uint32_t), offsets);
+                if (damage_dealt_address != 0)
+                {
+                    DWORD damage_dealt;
+                    external_memory_read(process_handle, (LPVOID)damage_dealt_address, &damage_dealt, sizeof(uint32_t));
+                    if (damage_dealt == 0)
+                        external_memory_patch(process_handle, (LPVOID)damage_dealt_address, &new_damage_dealt, sizeof(uint32_t));
+                }
+            } break;
+            default:
+                ExitProcess(1);
         }
         Sleep(1000);
     }
