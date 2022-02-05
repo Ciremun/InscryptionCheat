900e693aa228bae64ee646153b5ac0c069d6727a
Ciremun <Ciremun@yandex.ru>
2022-02-06 00:16:09 +0300

    debloat

--- build.bat
+++ build.bat
@@ -5,42 +5,7 @@ where /q cl || (
   exit /b 1
 )
 
-setlocal
+@echo on
 
-for %%i in (%*) do (
-    if "%%i" == "rebuild" (
-        set IC_REBUILD=1
-    ) else if "%%i" == "run" (
-        set IC_RUN=1
-    ) else (
-        echo ERROR: Unknown flag "%%i"
-        goto :eof
-    )
-)
-
-set ICEXE_BUILD=cl source/ic_inject.cpp /O2 /EHsc /Fe:ic.exe /std:c++17 /link /MACHINE:x86
-set ICDLL_BUILD=cl /DNDEBUG /O2 /EHsc /Fe:ic /nologo /W4 /Isource/include /Isource/include/imgui source/ic.cpp /link d3d11.lib source/lib/libMinHook.x86.lib /DLL /LTCG /MACHINE:x86 /OUT:ic.dll
-
-if defined IC_REBUILD goto :rebuild
-
-if not exist ic.exe call %ICEXE_BUILD%
-if not exist ic.dll call %ICDLL_BUILD%
-
-if defined IC_RUN goto :run
-
-goto :eof
-
-:rebuild
-
-call %ICEXE_BUILD%
-call %ICDLL_BUILD%
-
-if not defined IC_RUN goto :eof
-
-:run
-
-.\ic.exe
-
-endlocal
-
-:eof
+call cl source/ic_inject.cpp /DNDEBUG /O2 /EHsc /Fe:ic.exe /nologo /W4 /link /MACHINE:x86
+call cl /DNDEBUG /O2 /EHsc /Fe:ic /nologo /W4 /Isource/include /Isource/include/imgui source/ic.cpp /link d3d11.lib source/lib/libMinHook.x86.lib /LTCG /DLL /MACHINE:x86 /OUT:ic.dll
